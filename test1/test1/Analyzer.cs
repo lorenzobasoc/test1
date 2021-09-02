e06c5346c74dd9fb59acb832f8f6ced6fee1d5f9
lorenzobasoc <87638986+lorenzobasoc@users.noreply.github.com>
2021-09-02 12:53:34 +0200

    test finito

---
+++ test1/test1/Analyzer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    public class Analyzer
+    {
+        public static List<char> MostUsedChar(string[] words) {
+            List<char> mostUsed = new List<char>();
+            var occ = CharCount(words);
+            var max = 0;
+            for (int i = 0; i < occ.Length; i++) {          
+                if (char.IsWhiteSpace((char) i) || !IsLetter((char)i)) {
+                    continue;
+                }
+                if (occ[i] > max) {
+                    max = occ[i];
+                }
+            }
+            for (int j = 0; j < occ.Length; j++) {
+                if (occ[j] == max) {
+                    mostUsed.Add((char)j);
+                }
+            }
+            return mostUsed;
+        }
+
+        private static int[] CharCount(string[] words) {
+            var occ = new int[char.MaxValue+1];
+            foreach (var line in words) {
+                foreach (var c in line) {
+                    var newc = char.ToLower(c);
+                    occ[newc]++;
+                }
+            }
+            return occ;
+        }
+
+        public static string LongestSeq(string[] words) {
+            var arrayNoCharSeq = NotCharSeq(words);
+            var maxLength = 0;
+            var longest = "";
+            for (int i = 0; i < arrayNoCharSeq.Length; i++) {
+                if (arrayNoCharSeq[i] == null) {
+                    continue;
+                }
+                if (arrayNoCharSeq[i].Length > maxLength) {
+                    maxLength = arrayNoCharSeq[i].Length;
+                    longest = arrayNoCharSeq[i];
+                }
+            }
+            return longest;
+        }
+
+        private static string[] NotCharSeq(string[] words) {
+            var i = 0;
+            var noCharSeq = new string[char.MaxValue + 1];
+            foreach (var line in words) {
+                foreach (var c in line) {
+                    if (IsLetter(c) || char.IsWhiteSpace(c)) {
+                         continue;
+                    } else {
+                        noCharSeq[i] = "" + noCharSeq[i] + c;
+                    }
+                }
+                i++;
+            }
+            return noCharSeq;
+        }
+
+        private static bool IsLetter(char c) {
+            bool ris = false;
+            for (int i = (int)'a'; i <= (int)'z'; i++) {
+                if (c == (char)i) {
+                    ris = true;
+                }
+            }
+            for (int j = (int)'A'; j <= (int)'Z'; j++) {
+                if (c == (char)j) {
+                    ris = true;
+                }
+            }
+            return ris;
+        }
+
+        public static int OccSeq(string[] words) {
+            var longestSeq = LongestSeq(words);
+            var notCharSeq = NotCharSeq(words);
+            var count = 0;
+            for (int i = 0; i < notCharSeq.Length; i++) {
+                if (longestSeq.Equals(notCharSeq[i])) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public static int CountComments(string[] words) {
+            var count = 0;
+            foreach (var l in words) {
+                if (l.Contains("//")) {
+                    count++;
+                    continue;
+                }
+            }
+            return count;
+        }
+
+        public static int CountEnum(string[] words) {
+            var count = 0;
+            foreach (var l in words) {
+                if (l.Contains("enum")) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ test1/test1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace test1
+{
+    //     obiettvi:
+    // - Path del file
+    // - Lettera più usata 
+    // - Sequenza simboli più lunga
+    // - Occorrenze della Sequenza
+    // - Flag conta commenti
+    // - Conta commenti 
+    // - Conta enum
+    class Program
+    {
+        static void Main(string[] args) {
+            var path = GetPath();
+            if (!CheckPath(path)) {
+                Console.WriteLine("Il path del file non è valido.");
+                return;
+            }
+            IncludeComments();
+            var words = File.ReadAllLines(path);
+            //Analisi
+            var mostUsedChar = Analyzer.MostUsedChar(words);
+            var longestSeq = Analyzer.LongestSeq(words);
+            var occurenciesSeq = Analyzer.OccSeq(words);
+            var countComments = Analyzer.CountComments(words);
+            var countEnum = Analyzer.CountEnum(words);
+            Print(mostUsedChar, longestSeq, occurenciesSeq,countComments, countEnum);
+        }
+
+        public static string GetPath() {
+            Console.WriteLine("Inserisci un path.");
+            return Console.ReadLine();
+        }
+
+        public static bool CheckPath(string path) {
+            return (path == " " || !File.Exists(path) || Path.GetExtension(path) != ".cs");
+        }
+
+        public static bool IncludeComments() {
+            Again:
+            Console.WriteLine("Vuoi includere anche i commenti nell'analisi? \n- si \n - no");
+            var ris = Console.ReadLine();
+            if (ris == "si") {
+                return true;
+            }else if (ris == "no") {
+                return false;
+            } else {
+                Console.WriteLine("Inserisci un valore corretto.");
+                goto Again;
+            }
+        }
+
+        public static void Print(List<char> mostUsedChar, string longestSeq, int occurenciesSeq, int countComments, int countEnum){
+            Console.WriteLine("Il risultato dell'analisi:");
+            Console.WriteLine("Il carattere più usato è :");
+            PrintChar(mostUsedChar);
+            Console.WriteLine();
+            Console.WriteLine($"La sequenza di simboli più lunga è {longestSeq}");
+            Console.WriteLine($"Le occorrenze della sequenza più lunga sono {occurenciesSeq}");
+            Console.WriteLine($"Il numero di commenti è {countComments}");
+            Console.WriteLine($"Il numero delle enum è {countEnum}");
+        }
+
+        public static void PrintChar(List<char> list) {
+            foreach (var c in list) {
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
